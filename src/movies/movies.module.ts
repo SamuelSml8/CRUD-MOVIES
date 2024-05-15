c05b259e274fcebd7951672ec2500674d77e361f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-15 11:18:44 -0500

    Add Movie entity
    - Create Movie entity with fields: ID, name, description, and rating
    - Define entity properties and annotations for TypeORM
    
    This commit introduces the Movie entity, establishing the basic structure for storing movie data in the database.

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './controllers/movies.controller';
 import { MoviesService } from './services/movies.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movie.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Movie])],
+  providers: [MoviesService],
   controllers: [MoviesController],
-  providers: [MoviesService]
 })
 export class MoviesModule {}

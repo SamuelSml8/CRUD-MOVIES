5d776ac14ee678cb7a8c0a47d6fdc1eebfb0cc09
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-15 12:12:02 -0500

    Implement Movies Controller
    - Create controller for managing CRUD operations on movies
    - Define routes for CRUD operations within the controller
    - Implement methods to handle GET, POST, and DELETE requests
    - Utilize the movies service to perform database operations
    
    This commit introduces a movies controller to handle CRUD operations within the application.

--- src/movies/controllers/movies.controller.ts
+++ src/movies/controllers/movies.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { MoviesService } from '../services/movies.service';
+import { Movie } from '../entities/movie.entity';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get('all')
+  findAll(): Promise<Movie[]> {
+    return this.moviesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number): Promise<Movie> {
+    return this.moviesService.findById(id);
+  }
+
+  @Post('create')
+  create(@Body() movie: Movie): Promise<Movie> {
+    return this.moviesService.create(movie);
+  }
+
+  @Put('update/:id')
+  update(@Param('id') id: number, @Body() movie: Movie): Promise<Movie> {
+    return this.moviesService.update(id, movie);
+  }
+
+  @Delete('delete/:id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.moviesService.remove(id);
+  }
+}

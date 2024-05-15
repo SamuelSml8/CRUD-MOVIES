1a88a7c1552f294cc7539e93ee134a5879c6f497
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-15 11:59:25 -0500

    Handle movie not found exception in update method
    - Use findOne method to locate the movie by ID
    - If the movie is not found, throw an HttpException with a NOT_FOUND status code
    - Include a detailed error message in the exception response
    - Update the movie entity with the provided data and save it to the database
    
    This commit enhances the update method in the MoviesService to handle the scenario where the requested movie is not found, providing a more informative error response.

--- src/movies/services/movies.service.ts
+++ src/movies/services/movies.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Movie } from '../entities/movie.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(
+    @InjectRepository(Movie) private moviesRepository: Repository<Movie>,
+  ) {}
+
+  findAll(): Promise<Movie[]> {
+    return this.moviesRepository.find();
+  }
+
+  findById(id: number): Promise<Movie> {
+    return this.moviesRepository.findOneBy({ id });
+  }
+
+  create(movie: Movie): Promise<Movie> {
+    return this.moviesRepository.save(movie);
+  }
+
+  async update(id: number, updateMovieDto: Partial<Movie>): Promise<Movie> {
+    const movieFound = await this.moviesRepository.findOne({ where: { id } });
+    if (!movieFound)
+      throw new HttpException(
+        { ok: false, message: `Movie with ID ${id} not found`, data: null },
+        HttpStatus.NOT_FOUND,
+      );
+
+    Object.assign(movieFound, updateMovieDto);
+    return this.moviesRepository.save(movieFound);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.moviesRepository.delete({ id });
+  }
+}
